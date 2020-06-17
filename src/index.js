53bb5d1252ee814f18e1bcf2ee07c321cb6bbba4
Paul Varner <fuzzyray@gmail.com>
2020-06-17 02:36:45 -0500

    Inital App

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+/*
+import ReactHtmlParser from 'react-html-parser';
+import marked from 'marked';
+import DOMPurify from 'dompurify';
+*/
 
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <header>
+                <h1>Markdown Previewer</h1>
+                </header>
+                <div>
+                    <div>
+                        <h2>Editor</h2>
+                    </div>
+                    <div>
+                        <h2>Preview</h2>
+                    </div>
+                </div>
+            </div>
+        );
+    };
+}
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
